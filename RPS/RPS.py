fe6b9e032b81fbd1d25af33484d8a2539feb3bf8
Evan Klei <e32014@gmail.com>
2022-12-02 00:21:41 -0500

    Day 2: Writing some comments and doing some formatting for readability

--- RPS/RPS.py
+++ RPS/RPS.py
@@ -3,8 +3,18 @@ file = open("input.txt")
 myScore = 0
 winScore = 0
 
-scoreMat = {'A': {'X': 4, 'Y': 8, 'Z': 3}, 'B': {'X': 1, 'Y': 5, 'Z': 9}, 'C': {'X': 7, 'Y': 2, 'Z': 6}}
-winMat = {'A': {'X': 3, 'Y': 4, 'Z': 8}, 'B': {'X': 1, 'Y': 5, 'Z': 9}, 'C': {'X': 2, 'Y': 6, 'Z': 7}}
+# A and X are Rock, Rock is worth 1
+# B and Y are Paper, Paper is worth 2
+# C and Z are Scissors, Scissors are worth 3
+scoreMat = {'A': {'X': 4, 'Y': 8, 'Z': 3},
+            'B': {'X': 1, 'Y': 5, 'Z': 9},
+            'C': {'X': 7, 'Y': 2, 'Z': 6}}
+# X is Lose
+# Y is Tie
+# Z is Win
+winMat = {'A': {'X': 3, 'Y': 4, 'Z': 8},
+          'B': {'X': 1, 'Y': 5, 'Z': 9},
+          'C': {'X': 2, 'Y': 6, 'Z': 7}}
 
 for line in file:
     opp, me = line.strip().split()
