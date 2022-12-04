db6227c28360fe1af861c083806a272433911f01
Evan Klei <e32014@gmail.com>
2022-12-04 00:10:24 -0500

    Day 4: Really simple logic for checking if a point is between two other points on a 2d line.

---
+++ CleanupRange/CleanupRange.py
@@ -0,0 +1,19 @@
+import re
+
+file = open("input.txt")
+
+fullcount = 0
+anycount = 0
+for line in file:
+    cleanUp = line.strip().replace("-", ",")
+    min1, max1, min2, max2 = [int(elem) for elem in cleanUp.split(",")]
+    if min1 >= min2 and max1 <= max2:
+        fullcount += 1
+    elif min2 >= min1 and max2 <= max1:
+        fullcount += 1
+
+    if max1 >= max2 >= min1 or max1 >= min2 >= min1 or max2 >= max1 >= min2 or max2 >= min1 >= min2:
+        anycount += 1
+
+print(fullcount)
+print(anycount)
