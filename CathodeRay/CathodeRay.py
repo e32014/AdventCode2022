3a7b5d11c637840c991348e55f1c453da5e8f522
Evan Klei <e32014@gmail.com>
2022-12-10 00:42:33 -0500

    Day 10: Neat little cathode ray display. Tracked screen position with a seperate variable to eliminate some cross row issues. Also, used full blocks instead of # because they are easier to read.

---
+++ CathodeRay/CathodeRay.py
@@ -0,0 +1,40 @@
+file = open('input.txt')
+
+nums = []
+crt = ["." for i in range(0, 241)]
+cycles = 1
+
+command = file.readline().strip()
+register = 1
+screenPos = 1
+hold = False
+holdVal = None
+while command != "":
+    if -1 <= (register + 1 - screenPos) <= 1:
+        crt[cycles] = "█"
+    elif screenPos == 40 and -1 <= register - 39 <= 1:
+        crt[cycles] = "█"
+    if screenPos == 40:
+        screenPos = 0
+    if (cycles - 20) % 40 == 0:
+        nums.append(register * cycles)
+    if cycles == 240:
+        break
+    if hold:
+        register += holdVal
+        hold = False
+    else:
+        if command.startswith("addx"):
+            comm, val = command.split()
+            hold = True
+            holdVal = int(val)
+        command = file.readline().strip()
+    cycles += 1
+    screenPos += 1
+
+print(sum(nums[0:6]))
+
+for i in range(1, len(crt)):
+    print(crt[i], end="")
+    if i % 40 == 0:
+        print()
